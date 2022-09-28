e17d1ce4bc22b923fe600466fd1e07906b55d621
JaakkoKui <jaakko.kuivasniemi@hotmail.com>
2022-09-28 16:26:44 +0300

    Singleton done

---
+++ Singleton/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Singletonteht
+{
+    public class Main
+    {
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Singleton
+{
+    private static Singleton instance;
+
+    public static Singleton instance
+    {
+        get {
+            if instance == null {
+                instance = new Singleton()
+            }
+return instance;
+        }
+    }
+}
+
